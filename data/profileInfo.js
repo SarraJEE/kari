2d264a15b0c5c10970bcc9616bdaf106af3e854d
SarraJEE <bgrsarra@gmail.com>
2024-05-10 10:56:45 +0200

    first commit

---
+++ data/profileInfo.js
@@ -0,0 +1,11 @@
+
+
+
+export const profileInfo = {
+   name:'K Marine',
+   title:'',
+   imageSrcOne:'/assets/img/kari.jpg',
+   imageSrcTwo:'/assets/img/parsonal-info/parson-img-3.png',
+   imageSrcThree:'/assets/img/parsonal-info/parson-img-2.png',
+
+}
